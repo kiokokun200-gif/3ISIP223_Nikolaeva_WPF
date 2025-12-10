9aa5e10f1cf1dafb67058ce8b988826a35b3115d
kiokokun200-gif <kiokokun200@gmail.com>
2025-12-10 22:55:57 +0300

    Добавлены страницы

---
+++ 3ISIP223_Nikolaeva_WPF/Pages/ResultPage5.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3ISIP223_Nikolaeva_WPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ResultPage5.xaml
+    /// </summary>
+    public partial class ResultPage5 : Page
+    {
+        public ResultPage5()
+        {
+            InitializeComponent();
+        }
+    }
+}
