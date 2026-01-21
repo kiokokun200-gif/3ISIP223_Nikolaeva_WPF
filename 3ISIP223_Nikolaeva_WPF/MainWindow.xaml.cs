85393d6f8f7ffa1adccc936e416849d5eeede9f2
kiokokun200-gif <kiokokun200@gmail.com>
2026-01-21 14:42:36 +0300

    #

--- 3ISIP223_Nikolaeva_WPF/MainWindow.xaml.cs
+++ 3ISIP223_Nikolaeva_WPF/MainWindow.xaml.cs
@@ -22,65 +22,13 @@ namespace _3ISIP223_Nikolaeva_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Car car = new Car();    
+        
         public MainWindow()
         {
             InitializeComponent();
-            MainFrame.Navigate(new ModelPage1(car));
-
-            Progress.Value = 1;
-
-            BtnBack.IsEnabled = false;
+            
         }
 
-        private void BtnBack_Click(object sender, RoutedEventArgs e)
-        {
-            if(MainFrame.Content is ColorPage2)
-            {
-                MainFrame.Navigate(new ModelPage1(car));
-                Progress.Value = 1;
-            }
-            if(MainFrame.Content is TotalCostPage3)
-            {
-                MainFrame.Navigate(new ColorPage2(car));
-                Progress.Value = 2;
-            }
-            if(MainFrame.Content is CreditPage4)
-            {
-                MainFrame.Navigate(new TotalCostPage3(car));
-                Progress.Value = 3;
-            }
-            if(MainFrame.Content is ResultPage5)
-            {
-                MainFrame.Navigate(CreditPage4(car));
-                Progress.Value = 4;
-            }
-
-        }
-
-        private void BtnForward_Click(object sender, RoutedEventArgs e)
-        {
-            if(MainFrame.Content is ModelPage1)
-            {
-                MainFrame.Navigate(new ColorPage2(car));
-                Progress.Value = 2;
-            }
-            if(MainFrame.Content is ColorPage2)
-            {
-                MainFrame.Navigate(new TotalCostPage3(car));
-                Progress.Value = 3;
-            }
-            if(MainFrame.Content is TotalCostPage3)
-            {
-                MainFrame.Navigate(new CreditPage4(car));
-                Progress.Value = 4;
-            }
-            if(MainFrame.Content is CreditPage4)
-            {
-                MainFrame.Navigate(new ResultPage5(car));
-                Progress.Value = 5;
-            }
-                        
-        }
+       
     }
 }
