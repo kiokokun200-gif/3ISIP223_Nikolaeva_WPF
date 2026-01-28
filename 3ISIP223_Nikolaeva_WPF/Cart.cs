08e8149bfcba08f3aff1f041ab4f811ce9d12440
kiokokun200-gif <kiokokun200@gmail.com>
2026-01-28 15:56:46 +0300

    Класс корзина сделан

---
+++ 3ISIP223_Nikolaeva_WPF/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ISIP223_Nikolaeva_WPF
+{
+    internal class Cart
+    {
+        //public static List<Products> CartProducts = new List<Products>();
+        public int ID_Product { get; set; }
+        public int Quantity { get; set; }
+
+    }
+    //public static Cartlst {
+    //    }
+
+}
--- 3ISIP223_Nikolaeva_WPF/Pages/1PageProducts.xaml.cs
+++ 3ISIP223_Nikolaeva_WPF/Pages/1PageProducts.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace _3ISIP223_Nikolaeva_WPF.Pages
 {
     /// <summary>
@@ -23,6 +24,32 @@ namespace _3ISIP223_Nikolaeva_WPF.Pages
         public _1PageProducts()
         {
             InitializeComponent();
+            //DataContext = this;
+            List<Products>products = Core.Context.Products.ToList();
+            ProductsListBox.ItemsSource = products;
+
+            List<Cart>cart = 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        
+        { 
+            Button button = (Button)sender;
+            Products selectedProd = (Products)button.DataContext;
+
+            if (ProductsListBox.SelectedItem != null)
+            {
+                Products products = ProductsListBox.SelectedItem as Products;
+
+                if (Cart.CartProducts.FirstOrDefault(c => c.ID_Product == selectedProd.ID_Product) == null)
+                    Cart.CartProducts.Add(ProductsListBox.SelectedItem as Products);
+                else Cart.CartProducts
+            }
+        }
+
+        private void BtnCart_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
