3874ee24550ecefcdd45c05fc10191db03269761
kiokokun200-gif <kiokokun200@gmail.com>
2026-01-28 08:39:02 +0300

    БД

---
+++ 3ISIP223_Nikolaeva_WPF/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ISIP223_Nikolaeva_WPF
+{
+    internal class Core
+    {
+        public static OnlineShopEntities Context = new OnlineShopEntities();
+    }
+}
