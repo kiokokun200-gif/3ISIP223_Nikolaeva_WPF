0854e9dd7ab0174648b90012fc45069c0fb476bd
kiokokun200-gif <kiokokun200@gmail.com>
2026-02-04 20:55:24 +0300

    Выполнена страница фильма

---
+++ 3ISIP223_Nikolaeva_WPF/Seats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ISIP223_Nikolaeva_WPF
+{
+    public class Seats
+    {
+        public int RowNumber { get; set; }
+        public int SeatNumber { get; set; }   
+        public bool IsBooked { get; set; }
+
+    }
+}
