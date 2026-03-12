dce4de6bfe527a88c4994650bfe623f75a144820
kiokokun200-gif <kiokokun200@gmail.com>
2026-03-12 14:19:19 +0300

    фоны меняются

--- 3ISIP223_Nikolaeva_WPF/Pages/2MainGamePage.xaml.cs
+++ 3ISIP223_Nikolaeva_WPF/Pages/2MainGamePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _3ISIP223_Nikolaeva_WPF.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,24 @@ namespace _3ISIP223_Nikolaeva_WPF.Pages
         public _2MainGamePage()
         {
             InitializeComponent();
+            LoadPicture();
+
+
+        }
+
+        private void LoadPicture()
+        {
+            List<BitmapImage> backgroungimages = new List<BitmapImage>()
+            {
+                new BitmapImage( new Uri("pack://application:,,,/Images/Backgrounds/Loca1.jpeg")),
+                new BitmapImage( new Uri("pack://application:,,,/Images/Backgrounds/Loca2.png")),
+                new BitmapImage( new Uri("pack://application:,,,/Images/Backgrounds/Loca3.jpg")),
+                new BitmapImage( new Uri("pack://application:,,,/Images/Backgrounds/Loca4.jpg")),
+                new BitmapImage( new Uri("pack://application:,,,/Images/Backgrounds/Loca5.jpg"))
+            };
+
+            int n  = Raaandom.GetRandomInt(0, backgroungimages.Count - 1);
+            ImagBr.ImageSource = backgroungimages[n];
         }
     }
 }
