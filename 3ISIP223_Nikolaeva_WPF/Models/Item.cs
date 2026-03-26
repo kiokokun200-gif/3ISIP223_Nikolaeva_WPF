78851e5954ae24a78de1f7fcd1b4bd6b177f2ffc
kiokokun200-gif <kiokokun200@gmail.com>
2026-03-26 10:31:39 +0300

    броня

--- 3ISIP223_Nikolaeva_WPF/Models/Item.cs
+++ 3ISIP223_Nikolaeva_WPF/Models/Item.cs
@@ -12,11 +12,14 @@ namespace _3ISIP223_Nikolaeva_WPF.Models
         public int Attack { get; set; }
         public int Defense { get; set; }
 
-        public Item(string name, int attack, int defense)
+        public string Image { get; set; }
+
+        public Item(string name, int attack, int defense, string image )
         {
             Name = name;
             Attack = attack;
             Defense = defense;
+            Image = image;
         }
 
         public void DisplayStats()
