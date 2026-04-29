d914949fe09bf5fdb27f293880cbc97113b0c3b2
kiokokun200-gif <kiokokun200@gmail.com>
2026-04-29 16:00:00 +0300

    страница каталога

---
+++ 3ISIP223_Nikolaeva_WPF/Pages/1PageMain.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3ISIP223_Nikolaeva_WPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для _1PageMain.xaml
+    /// </summary>
+    public partial class _1PageMain : Page
+    {
+        public _1PageMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCatalogBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnListBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdmin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAuthor_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnProfile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 3ISIP223_Nikolaeva_WPF/Pages/2PageCatalog.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3ISIP223_Nikolaeva_WPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для _2PageCatalog.xaml
+    /// </summary>
+    public partial class _2PageCatalog : Page
+    {
+        public _2PageCatalog()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBoxBooks_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void BtnAddToList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
